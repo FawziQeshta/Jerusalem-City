347d2e1e2d606d5dbf69ba6df8960a5c81813414
FawziQeshta <45800534+FawziQeshta@users.noreply.github.com>
2021-05-06 02:45:42 +0300

    add topic is done

--- app/src/main/java/com/iug/jerusalem_city/ui/add_topic/AddTopicActivity.java
+++ app/src/main/java/com/iug/jerusalem_city/ui/add_topic/AddTopicActivity.java
@@ -1,16 +1,32 @@
 package com.iug.jerusalem_city.ui.add_topic;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.iug.jerusalem_city.R;
 import com.iug.jerusalem_city.databinding.ActivityAddTopicBinding;
+import com.iug.jerusalem_city.utils.Constants;
 
 public class AddTopicActivity extends AppCompatActivity {
 
     private ActivityAddTopicBinding binding;
+
+    private final int IMAGE_REC_CODE = 200;
+
+    private Uri filePath = null;
+
+    private AddTopicPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +34,26 @@ public class AddTopicActivity extends AppCompatActivity {
         binding = ActivityAddTopicBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.imgOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasPermissionsGranted()) {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent.createChooser(intent, "Select image"), IMAGE_REC_CODE);
+                }
+            }
+        });
 
+        presenter = new AddTopicPresenter(this);
+
+        binding.btnAddTopic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkInputs();
+            }
+        });
 
         binding.addTopicToolbar.setNavigationIcon(R.drawable.ic_back);
         binding.addTopicToolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -29,4 +64,54 @@ public class AddTopicActivity extends AppCompatActivity {
         });
 
     }
+
+    private void checkInputs() {
+        if (binding.etNewTopic.getText().toString() == null) {
+            Toast.makeText(AddTopicActivity.this, "الرجاء ادخال نص الموضوع.", Toast.LENGTH_SHORT).show();
+        } else if (getSectionTitle() == null) {
+            Toast.makeText(AddTopicActivity.this, "الرجاء اختيار صنف للموضوع.", Toast.LENGTH_SHORT).show();
+        } else if (filePath == null) {
+            Toast.makeText(AddTopicActivity.this, "الرجاء اضافة صورة للموضوع.", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!binding.etNewTopic.getText().toString().trim().isEmpty()) {
+                presenter.sendUserTopic(binding.etNewTopic.getText().toString().trim(), getSectionTitle(), filePath);
+            } else {
+                Toast.makeText(AddTopicActivity.this, "الرجاء ادخال نص الموضوع.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean hasPermissionsGranted() {
+        int writePermission = ContextCompat.checkSelfPermission(AddTopicActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED) {
+            String[] params = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(AddTopicActivity.this, params, IMAGE_REC_CODE);
+            return false;
+
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REC_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            binding.imgOpenGallery.setImageURI(filePath);
+        }
+    }
+
+    private String getSectionTitle() {
+        if (binding.radioButtonClimate.isChecked()) {
+            return binding.radioButtonClimate.getText().toString();
+        } else if (binding.radioButtonHistory.isChecked()) {
+            return binding.radioButtonHistory.getText().toString();
+        } else if (binding.radioButtonTouristicMonuments.isChecked()) {
+            return binding.radioButtonTouristicMonuments.getText().toString();
+        }
+        return null;
+    }
+
 }
--- app/src/main/java/com/iug/jerusalem_city/ui/add_topic/AddTopicPresenter.java
+++ app/src/main/java/com/iug/jerusalem_city/ui/add_topic/AddTopicPresenter.java
@@ -1,23 +1,106 @@
 package com.iug.jerusalem_city.ui.add_topic;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.iug.jerusalem_city.utils.Constants;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddTopicPresenter {
 
     private Context context;
 
+    private FirebaseFirestore db;
+    private StorageReference storageRef;
+    private ProgressDialog progressDialog;
+
+    String imgStoragePath;
+
+    private static final String TAG = "AddTopicPresenter";
+
     public AddTopicPresenter(Context context) {
         this.context = context;
+        db = FirebaseFirestore.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        dialogInit();
     }
 
-    public void sendUserTopic(String textTopic, String choiceText) {
+    private void dialogInit() {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("جار إضافة الموضوع...");
+    }
 
+    public void sendUserTopic(String textTopic, String choiceText, Uri filePath) {
+        progressDialog.show();
+
+        imgStoragePath = "images/" + System.currentTimeMillis() + ".jpg";
+
+        StorageReference addNewImagesRef = storageRef.child(imgStoragePath);
+        addNewImagesRef.putFile(filePath).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e(TAG, "onComplete: " + task.getException());
+                    return;
+                }
+                Log.d(TAG, "Uploaded Image");
+                uploadUserTopic(textTopic, choiceText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFailure: " + e.getMessage());
+            }
+        });
+
+    }
+
+    private void uploadUserTopic(String textTopic, String choiceText) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("text", textTopic);
+        map.put("imageUrl", imgStoragePath);
+        map.put("videoUrl", "");
+        map.put("hasVideo", false);
+
+        DocumentReference bulletinRef = db.collection(Constants.KEY_MAIN_COLLECTION).document(Constants.KEY_MAIN_COLLECTION_ID);
+
+        bulletinRef.collection(getCollectionUserChoosed(choiceText))
+                .add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        progressDialog.dismiss();
+                        Toast.makeText(context, "تم اضافة الموضوع بنجاح", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(context, "حدث خطأ أثناء حفظ الموضوع", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private String getCollectionUserChoosed(String text) {
-        switch (text){
+        switch (text) {
             case "تاريخ المدينة":
                 return Constants.KEY_HISTORY_COLLECTION;
 
