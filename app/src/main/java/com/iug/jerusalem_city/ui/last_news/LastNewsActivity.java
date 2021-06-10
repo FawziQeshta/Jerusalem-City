c5c59cc4ccb2e14cb05e76e2e270e46b799ce87d
FawziQeshta <45800534+FawziQeshta@users.noreply.github.com>
2021-06-10 20:09:02 +0300

    add favorite feature

--- app/src/main/java/com/iug/jerusalem_city/ui/last_news/LastNewsActivity.java
+++ app/src/main/java/com/iug/jerusalem_city/ui/last_news/LastNewsActivity.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LastNewsActivity extends AppCompatActivity implements LastNewsPresenter.LastNewsListener {
-
+// fawzi qeshta
     private ActivityLastNewsBinding binding;
 
     private List<Article> data;
